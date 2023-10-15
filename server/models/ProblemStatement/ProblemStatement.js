bf64a3c7f95de4637f1ef0a67ddf232890482376
adii21-ux <adityasangave2112@gmail.com>
2023-10-15 13:29:53 +0530

    Modified problem statement schema

--- server/models/ProblemStatement/ProblemStatement.js
+++ server/models/ProblemStatement/ProblemStatement.js
@@ -21,16 +21,19 @@ const ProblemStatement = new mongoose.Schema({
     example_output : {
         type : String,
     },
-    testcases : [{
-        input : String,
-        Output : String
-    }],
     required_testcase_input : { // Since every testcase ip/op is string this helps identify to which dtype testcase ip/op should be converted
         type : String
     },
     required_testcase_output : {
         type : String
-    }
+    },
+    testcases : [{
+        input : String,
+        Output : String
+    }]
+},
+{
+    timestamps:true
 })
 
 const Problem = new mongoose.model('ps', ProblemStatement)
