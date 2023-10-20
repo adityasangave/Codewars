20f357396dc8038157a0cf6bf495a50e59f03e01
adii21-ux <adityasangave2112@gmail.com>
2023-10-20 23:03:46 +0530

    Added login support

---
+++ client/src/Components/Auth/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../Context/AuthContext';
+
+function Login() {
+  const history = useNavigate();
+  const { login } = useAuth();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/auth/login', formData);
+
+      // Handle success: Save the JWT token in local storage
+      const token = response.data.token;
+      const id = response.data.user.id;
+      const name = response.data.user.name;
+      const email = response.data.user.email;
+
+      login({ 'id': id, 'name': name, 'email': email, 'token': token })
+
+      console.log('Login successful:', response.data);
+      history('/')
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>CodeWars</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
