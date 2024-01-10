8ac4e2d66dca9df72e3604981647be90523511ab
adii21-ux <adityasangave2112@gmail.com>
2024-01-11 03:09:26 +0530

    Update : Added support for select language

--- client/src/Components/Playground/Playground.js
+++ client/src/Components/Playground/Playground.js
@@ -6,10 +6,18 @@ import './Style.css'
 import axios from 'axios'
 
 function Playground() {
+    const [selectedLanguage, setSelectedLanguage] = useState({});
+
     const [code, setCode] = useState();
     const { state } = useLocation()
     const problem = state.selectedProblem;
     let token = ''
+
+    const languageIds = {
+        'python' : "71",
+        'c++' : "53",
+        'java' : "91"
+    }
 
     const options = {
         url: 'https://judge0-ce.p.rapidapi.com',
@@ -22,7 +30,7 @@ function Playground() {
     };
 
     const reqBody = {
-        "source_code": "print(\"hell0\")",
+        "source_code": `${code}`,
         "language_id": "71",
         "number_of_runs": null,
         "stdin": "Judge0",
@@ -38,6 +46,11 @@ function Playground() {
         "max_file_size": null,
         "enable_network": null
     }
+
+    const handleSelectChange = (event) => {
+        const lang = event.target.value;;
+        setSelectedLanguage({'lang' : lang, 'id' : languageIds[lang]});
+    };
 
     const handleCodeChange = (newCode) => {
         console.log(newCode)
@@ -67,7 +80,7 @@ function Playground() {
         waitFor5Seconds(function () {
             axios(`${options.url}/submissions/${token}`, {
                 method: "GET",
-                headers : options.headers,
+                headers: options.headers
             })
                 .then((response) => {
                     console.log(response)
@@ -84,6 +97,17 @@ function Playground() {
                 <ProblemDescription problem={problem} />
             </div>
             <div style={{ width: '65%', height: '100%' }}>
+                <div>
+                    <select id="languages" value={selectedLanguage} onChange={handleSelectChange} style={{ border:'none' ,borderRadius: '7px', fontFamily: 'Poppins', padding: '5px', margin:'3px' }}>
+                        <option value="c++">C++</option>
+                        <option value="java">Java</option>
+                        <option value="python">Python</option>
+                    </select>
+
+                    {selectedLanguage['lang'] && (
+                        <p>You selected: {selectedLanguage['lang']}</p>
+                    )}
+                </div>
                 <CodeEditor value={code} handleOnChange={handleCodeChange} testcases={problem.testcases} handleCodeRun={handleCodeRun} />
             </div>
         </div>
