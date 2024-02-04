e2d53b07b8fe706af08293b5e8df3a50ee0b38a8
adii21-ux <adityasangave2112@gmail.com>
2024-02-05 01:05:54 +0530

    Update : Added Select/Unselect problem event in websocket

--- client/src/Components/Room/ProblemList.js
+++ client/src/Components/Room/ProblemList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const ProblemList = ({ user, room, problems, handleProblemClicked, handleProblemSelect }) => {
+const ProblemList = ({ user, room, problems, selected,handleProblemClicked, handleProblemSelect }) => {
     return (
         <div className="problem-section">
             {problems.map((problem) => (
                 <div key={problem._id} className="problem"
-                    style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor:'pointer' }}>
+                    style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor: 'pointer' }}>
                     <div className="problem-info" onClick={() => handleProblemClicked(problem._id)}>
                         <div className='problem-name'>{problem.name}</div>
                         <div className='problem-description'>{problem.description}</div>
                         <div className='solve-time'>{problem.time_to_solve} minutes</div>
                     </div>
-                    {room.created_by === user.id && <div><button onClick={() => handleProblemSelect(problem._id)}>Select</button></div>}
+                    {room.created_by === user.id && <div><button onClick={()=>handleProblemSelect(problem._id)}>
+                        {selected ? 'Unselect' : 'Select'}
+                    </button></div>}
                 </div>
             ))}
 
